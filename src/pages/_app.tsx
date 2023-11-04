e35895f01bd83802b435fd6794dafaef4623ead5
Franco Fusaro <ffusaro94@gmail.com>
2023-11-04 12:02:48 -0300

    Modified to use wagmi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,20 @@ import { WagmiConfig } from "wagmi";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import {
-	mainnet,
-	sepolia,
-	hardhat,
+  mainnet,
+  sepolia,
+  hardhat,
 } from "wagmi/chains";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import Head from "next/head";
+import createEmotionCache from "@/utils/createEmotionCache";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from '@/styles/theme';
 
 const chains = [
-	mainnet,
-	sepolia,
-	hardhat
+  mainnet,
+  sepolia,
+  hardhat
 ];
 
 // 1. Get projectID at https://cloud.walletconnect.com
@@ -21,29 +26,46 @@ const chains = [
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
 
 const metadata = {
-	name: "Next Starter Template",
-	description: "A Next.js starter template with Web3Modal v3 + Wagmi",
-	url: "https://web3modal.com",
-	icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  name: "Next Starter Template",
+  description: "A Next.js starter template with Web3Modal v3 + Wagmi",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 
 createWeb3Modal({ wagmiConfig, projectId, chains });
 
-export default function App({ Component, pageProps }: AppProps) {
-	const [ready, setReady] = useState(false);
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-	useEffect(() => {
-		setReady(true);
-	}, []);
-	return (
-		<>
-			{ready ? (
-				<WagmiConfig config={wagmiConfig}>
-					<Component {...pageProps} />
-				</WagmiConfig>
-			) : null}
-		</>
-	);
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  return (
+    <>
+      {ready ? (
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <WagmiConfig config={wagmiConfig}>
+              <Component {...pageProps} />
+            </WagmiConfig>
+          </ThemeProvider>
+        </CacheProvider>
+      ) : null}
+    </>
+  );
 }
---
+++ src/pages/projects/create.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function ProjectCreate() {
+  return (
+    <>
+      <Head>
+        <title>CryptoFundMe</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
